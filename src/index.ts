1a29118bf1c43ce341503a31658cfaaf26cd52a0
Roger Chappell <rogerchappel@users.noreply.github.com>
2026-05-05 06:35:14 +1000

    feat: export library API

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from './types.js';
+export * from './errors.js';
+export * from './metadata.js';
+export * from './pack.js';
+export * from './unpack.js';
+export * from './registry.js';
+export * from './compat.js';
